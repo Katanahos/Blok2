79bbe11c720d376cb900a6d7b6b2a30f7f82fc7f
Katanahos <katanahos@gmail.com>
2020-09-03 23:03:44 +0300

    Answers

--- src/Task1.java
+++ src/Task1.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 public class Task1 {
 
     public static void main(String[] args) {
+        ArrayList<Integer> A = new ArrayList<>();
 
 	// write your code here
     }
