4b82e53cd4bf81f255da82fb13f201b086ee64ee
Katanahos <katanahos@gmail.com>
2020-09-03 23:21:59 +0300

    Answers

---
+++ src/Task2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int count = 0;
+        double[] mass = new double[size];
+        mass[0] = scanner.nextDouble(); // первый член последовательности
+        double Z = scanner.nextDouble();
+        double change = scanner.nextDouble(); //простой пример изменения ряда
+
+        for (int i = 1; i < mass.length; i++) {
+            mass[i] = (mass[i] - 1) + change;
+        }
+
+        for (int i = 0; i < mass.length; i++) {
+            if(mass[i]<Z){
+                mass[i] = Z;
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
