ee031a6198010ff0a6cfcdf617930c7948f2efac
Katanahos <katanahos@gmail.com>
2020-09-03 23:10:39 +0300

    Answers

--- src/Task1.java
+++ src/Task1.java
@@ -1,10 +1,25 @@
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Task1 {
 
     public static void main(String[] args) {
-        ArrayList<Integer> A = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int K = scanner.nextInt();
+        int[] A = new int[N];
+        int sum = 0;
 
-	// write your code here
+        for (int i = 0; i < N; i++) {
+            A[i] = scanner.nextInt();
+        }
+
+        for (int j : A) {
+            if (j % K == 0) {
+                sum += j;
+            }
+        }
+        System.out.println(sum);
+
+        // write your code here
     }
 }
